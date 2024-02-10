6a71604fed6ed1bf9939d8deb26944e6eb698b4a
Rashid Khan <rashidkhan.rk06@gmail.com>
2024-02-10 19:03:41 +0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='neatpy',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        # add other dependencies here if needed
+    ],
+)
